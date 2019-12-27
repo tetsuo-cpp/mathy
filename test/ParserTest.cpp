eef466f2b6662489f6712214b224066fbf9fad8f
Alex Cameron <ascottcameron@gmail.com>
2019-12-27 22:07:28 +1100

    Add helper functions and macros for comparing trees.

--- test/ParserTest.cpp
+++ test/ParserTest.cpp
@@ -24,6 +24,41 @@ private:
   size_t pos;
 };
 
+void compare(const IAst &lhs, const IAst &rhs);
+
+void compareImpl(const VarDecl &lhs, const VarDecl &rhs) {
+  REQUIRE(lhs.name == rhs.name);
+  compare(*lhs.rhs, *rhs.rhs);
+}
+
+void compareImpl(const BinOp &lhs, const BinOp &rhs) {
+  REQUIRE(lhs.op == rhs.op);
+  compare(*lhs.lhs, *rhs.lhs);
+  compare(*lhs.rhs, *rhs.rhs);
+}
+
+void compareImpl(const Number &lhs, const Number &rhs) {
+  REQUIRE(lhs.value == rhs.value);
+}
+
+#define COMPARE_HELPER(type, variable)                                         \
+  const auto *variable = dynamic_cast<const type *>(&lhs);                     \
+  if (variable) {                                                              \
+    const auto *variable##Rhs = dynamic_cast<const type *>(&rhs);              \
+    INFO("checking that rhs is of type " #type);                               \
+    REQUIRE(variable##Rhs);                                                    \
+    return compareImpl(*variable, *variable##Rhs);                             \
+  }
+
+void compare(const IAst &lhs, const IAst &rhs) {
+  COMPARE_HELPER(VarDecl, varDecl);
+  COMPARE_HELPER(BinOp, binOp);
+  COMPARE_HELPER(Number, number);
+  // Means we haven't added a helper for the type of lhs.
+  INFO("encountered unexpected ast type");
+  REQUIRE(false);
+}
+
 std::unique_ptr<IAst> createBinOp(char op, int lhs, int rhs) {
   return std::make_unique<BinOp>(op, std::make_unique<Number>(lhs),
                                  std::make_unique<Number>(rhs));
@@ -31,6 +66,11 @@ std::unique_ptr<IAst> createBinOp(char op, int lhs, int rhs) {
 
 } // namespace
 
-TEST_CASE("parser handles addition") { REQUIRE(true); }
+TEST_CASE("parser handles addition") {
+  auto var = createBinOp('+', 1, 2);
+  static_cast<void>(var);
+  compare(Number(1), Number(1));
+  compare(*var, Number(1));
+}
 
 } // namespace mathy::test
